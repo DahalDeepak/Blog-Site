eca43dc44cacbe111d90e875eb58555eef1c5d90
Deepak Dahal <Deepak9dahal@gmail.com>
2023-02-04 21:51:21 +0545

    connecting to databse

--- server/src/index.js
+++ server/src/index.js
@@ -5,12 +5,13 @@ const app = require("express")();
 require("dotenv").config();
 
 const port = process.env.PORT;
-
+const userName = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
 app.listen(port, () => {
   console.log(`server is running ${port}`);
 });
 
-const URI =`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-xv6zsch-shard-00-00.set2im1.mongodb.net:27017,ac-xv6zsch-shard-00-01.set2im1.mongodb.net:27017,ac-xv6zsch-shard-00-02.set2im1.mongodb.net:27017/?ssl=true&replicaSet=atlas-9ix274-shard-0&authSource=admin&retryWrites=true&w=majority`
+const URI =`mongodb://${userName}:${password}@ac-xv6zsch-shard-00-00.set2im1.mongodb.net:27017,ac-xv6zsch-shard-00-01.set2im1.mongodb.net:27017,ac-xv6zsch-shard-00-02.set2im1.mongodb.net:27017/?ssl=true&replicaSet=atlas-9ix274-shard-0&authSource=admin&retryWrites=true&w=majority`
 try{mongoose.connect(URI).then(()=>console.log("Connected to data base"))}
 catch(erro){
   console.log("error occure",error)
